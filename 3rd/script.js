7a485a69364c04c02f35c72a987138116b5eb2ce
Sourabh Manawat <sourabhmanawat@gmail.com>
2023-10-27 21:01:08 +0530

    pushing 3rd project script file

---
+++ 3rd/script.js
@@ -0,0 +1,15 @@
+const draggableList = document.querySelector("#draggable-list");
+const check = document.querySelector(".check");
+// console.log(draggableList, check);
+
+const richestPersons = [
+  "Elon Musk",
+  "Bernard Arnault & family",
+  "Jeff Bezos",
+  "Warren Buffett",
+  "Bill Gates",
+  "Larry Page",
+  "Mark Zuckerberg",
+  "Steve Ballmer",
+  "Sergey Brin",
+];
