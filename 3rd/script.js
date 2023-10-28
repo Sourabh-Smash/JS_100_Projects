d0b6e0d08a1d53c0ef56a141755270a7dad9e430
Sourabh Manawat <sourabhmanawat@gmail.com>
2023-10-28 19:36:38 +0530

    pushing 3rd project style file and created html code using js

--- 3rd/script.js
+++ 3rd/script.js
@@ -13,3 +13,32 @@ const richestPersons = [
   "Steve Ballmer",
   "Sergey Brin",
 ];
+
+// console.log(richestPersons);
+
+const listItems = [];
+
+updateList();
+
+function updateList() {
+  [...richestPersons]
+    .map((a) => ({ value: a, sort: Math.random() }))
+    .sort((a, b) => a.sort - b.sort)
+    .map((a) => a.value)
+    .forEach((person, idx) => {
+      // console.log(person);
+      const listItem = document.createElement("li");
+
+      // listItem.classList.add('over')
+      listItem.setAttribute("data-index", idx);
+      listItem.innerHTML = `
+    <span class="number">${idx + 1}</span>
+    <div class="draggable" draggable=true >
+    <p class="person-name">${person}</p>
+    <i class="fas fa-grip-lines"></i>
+    </div>`;
+
+      listItems.push(listItem);
+      draggableList.appendChild(listItem);
+    });
+}
